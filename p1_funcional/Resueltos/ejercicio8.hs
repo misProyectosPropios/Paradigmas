d4b6b40bf3d326796746a6f1f27e7fc2362dd8e4
misProyectosPropios <inakifrutos@outlook.com>
2025-03-29 09:17:50 -0300

    MODIFY ejercicio8 to become more efficient

--- p1_funcional/Resueltos/ejercicio8.hs
+++ p1_funcional/Resueltos/ejercicio8.hs
@@ -26,11 +26,16 @@ sumaVector (x:xs) (y:ys) = x + y : sumaVector xs ys
 -- Voy a asumir que xs e ys tienen la misma longitud
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use bimap" #-}
-sumaVector :: [Int] -> [Int] -> [Int]
-sumaVector xs ys = snd $ foldr (\x (listaYs, res) -> (init listaYs, last listaYs + x : res)) ( ys, []) xs
+-- sumaVector :: [Int] -> [Int] -> [Int]
+-- sumaVector xs ys = snd $ foldr (\x (listaYs, res) -> (init listaYs, last listaYs + x : res)) ( ys, []) xs
 
+--sumaMat :: [[Int]] -> [[Int]] -> [[Int]]
+--sumaMat xs ys = snd $ foldr (\x (listaYs, res) -> (init listaYs, sumaVector x (head listaYs) : res)) (ys, []) xs
+
+
+-- Complejidad menor, ya que init es O(n), pero si hago un reverse en ys va a funcioanr solo una vez O(n)
 sumaMat :: [[Int]] -> [[Int]] -> [[Int]]
-sumaMat xs ys = snd $ foldr (\x (listaYs, res) -> (init listaYs, 
-                                        if res == [[]]
-                                        then [sumaVector x (head listaYs)]
-                                        else sumaVector x (head listaYs) : res)) (ys, [[]]) xs
+sumaMat xs ys = snd $ foldr (\x (listaYs, res) -> (tail  listaYs, sumaVector x (head listaYs) : res)) (reverse ys, []) xs
+
+sumaVector :: [Int] -> [Int] -> [Int]
+sumaVector xs ys = snd $ foldr (\x (listaYs, res) -> (tail listaYs, head listaYs + x : res)) (reverse ys, []) xs
