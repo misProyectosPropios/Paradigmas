cbf586cbd4121a60096be6940b67b310aba0749b
misProyectosPropios <inakifrutos@outlook.com>
2025-03-30 12:15:54 -0300

    Llegué a lograr que sea usando recursion explicita

--- p1_funcional/Resueltos/ejercicio11.hs
+++ p1_funcional/Resueltos/ejercicio11.hs
@@ -1,1 +1,32 @@
+import GHC.Exts.Heap (GenClosure(MutVarClosure))
+ {- 
+ Definir el esquema de recursión estructural para el siguiente tipo:
+data Polinomio a = X
+| Cte a
+| Suma (Polinomio a) (Polinomio a)
+| Prod (Polinomio a) (Polinomio a)
+Luego usar el esquema definido para escribir la función evaluar :: Num a => a -> Polinomio a -> a
+que, dado un número y un polinomio, devuelve el resultado de evaluar el polinomio dado en el número dado.
  
+ -}
+data Polinomio a = X | Cte a | Suma (Polinomio a) (Polinomio a) | Prod (Polinomio a) (Polinomio a)
+
+foldPol :: (Polinomio a -> b -> b) -> b -> Polinomio a -> b 
+foldPol f x X = x
+foldPol f x (Cte valor) = x
+foldPol f x y = f y (foldPol f x y)
+{-
+evaluar :: Num a => a -> Polinomio a -> a
+evaluar evaluar pol = evaluar
+
+-}
+{-
+Con recursión
+
+evaluar :: Num a => a -> Polinomio a -> a
+evaluar _ (Cte constante) = constante
+evaluar valor (Suma x y) = evaluar valor x + evaluar valor y
+evaluar valor (Prod x y) = evaluar valor x * evaluar valor y
+evaluar valor _ = valor
+
+-}
